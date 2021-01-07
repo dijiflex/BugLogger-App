a808af8e555226011a728bd35495ba4ff77958c7
dijiflex <dijiflex@gmail.com>
2021-01-07 17:09:45 +0300

    Created first table component for rendering logitems

---
+++ src/components/LogItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LogItem = () => {
+    return (
+        <tr>
+            <td>Something</td>
+            <td>Something</td>
+            <td>Something</td>
+            <td>Something</td>
+            <td>Something</td>
+        </tr>
+    )
+}
+
+export default LogItem
