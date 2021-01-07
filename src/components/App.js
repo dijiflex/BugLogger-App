29b3dc6b83733463c1767fba17de0893ffffee1b
dijiflex <dijiflex@gmail.com>
2021-01-07 21:11:06 +0300

    Implemented displaying of alerts

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@
 import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
+import Alert from 'react-bootstrap/Alert';
+
 import LogItem from "./LogItem";
 import AddLogItem from "./AddLogItem"; 
+
 
 const App = () => {
     const [logs, setLogs] = useState([
@@ -30,16 +33,46 @@ const App = () => {
         }
     ]);
 
+    const [alert, setAlert] = useState({
+        show: false,
+        message: '',
+        variant: 'success'
+
+    })
+
     function addItem (item) {
+        if(item.text === '' || item.user === '', item.priority === ''){
+            showAlert('Please Enter all fields', 'danger')
+            return false
+        }
+
     item._id = Math.floor(Math.random() * 90000) +1000
     item.created = new Date().toString()
-      setLogs([...logs, item])
+      setLogs([...logs, item]);
+      showAlert('Log Added')
+    }
+
+    function showAlert(message, variant = 'success', seconds = 3000){
+        setAlert({
+            show: true,
+            message,
+            variant
+        })
+
+        setTimeout(() => {
+            setAlert({
+                show: false,
+                message,
+                variant: 'success'
+            })
+        }, seconds);
     }
 
 
 	return (
 		<Container>
             <AddLogItem addItem={addItem}/>
+            { alert.show && <Alert variant={alert.variant}>{alert.message}</Alert>}
 			<Table>
                 <thead>
                     <tr>
